8dd88cd6ffbeb014b84542b158572c0e9b3cecb9
Kevin.M <kema2501@student.miun.se>
2026-04-07 13:41:22 +0200

    Felhantering lagring i localStorage + lagt till remove funktionalitet

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,42 +1,27 @@
-getStoredCourses ();
+addEventListener("DOMContentLoaded", () => {
 
-//Interface
+    getStoredCourses ()
+
+    const form = document.querySelector<HTMLFormElement>(".add-new-course");
+    if (!form) return;
+    form.addEventListener("submit", addCourse);
+
+});
+
+//Interface för kurs
 interface Course {
     code: string;
     name: string;
-    progression: string;
+    progression: Progression;
     syllabus: string;
 }
 
-// Funktion för att skriva ut kurs
-function printCourseDetails (course: Course):void {
-    const courseTable = document.querySelector("tbody") as HTMLElement;
+// Enums fungerade inte (erasableSyntaxOnly)
+type Progression = "A" | "B" | "C";
 
-    if(courseTable) {
-        courseTable.innerHTML += `
-        <tr>
-            <td>${course.code}</td>
-            <td>${course.name}</td>
-            <td>${course.progression}</td>
-            <td>${course.syllabus}</td>
-            <td><span class="material-symbols-outlined remove">delete</span></td>
-        </tr>
-        `;
-    }
-}
-
-// Hämta DOM-element för formulär och användardetaljer
-const courseForm = document.querySelector(".add-new-course") as HTMLFormElement;
-
-// Lägg till händelselyssnare på formuläret
-if(courseForm) {
-    courseForm.addEventListener("submit", (event) => {
+function addCourse(event: SubmitEvent) {
     event.preventDefault();
-    createCourse();
-    });
-}
 
-function createCourse():void {
     // Hämta värden från formuläret
     const codeInputEl = document.querySelector("#course-code") as HTMLInputElement;
     const nameInputEl = document.querySelector("#course-name") as HTMLInputElement;
@@ -48,13 +33,18 @@ function createCourse():void {
     if(errorMessageEl) { errorMessageEl.textContent = ""; }
     let error: string[] = [];
 
+    // Skapa felmeddelanden
     if(codeInputEl.value.length < 2) {
         error.push("Fyll i kurskod");
     } if (nameInputEl.value.length < 2) {
         error.push("Fyll i kursnamn");
     } if(syllabusInputEl.value.length < 2) {
         error.push("Fyll i länk till kursplan");
+    }if (!progressionInputEl) {
+        error.push("Ange progression");
     }
+    let progressionValue = progressionInputEl.value as Progression;
+    
 
     // Utskrift av felmeddelande
     if(errorMessageEl && error.length > 0) {
@@ -64,21 +54,58 @@ function createCourse():void {
         return;
     }
 
-  // Skapa ett kursobjekt
-  const newCourse: Course = {
-    code: codeInputEl.value,
-    name: nameInputEl.value,
-    progression: progressionInputEl.value,
-    syllabus: syllabusInputEl.value,
-  };
-
-    //Lagra i LocalStorage
-    const storedCourses: Course[] = JSON.parse(localStorage.getItem("courses") || "[]");
-    storedCourses.push(newCourse);
-    localStorage.setItem("courses", JSON.stringify(storedCourses));
+    // Skapa ett kursobjekt
+    const newCourse: Course = {
+        code: codeInputEl.value,
+        name: nameInputEl.value,
+        progression: progressionValue,
+        syllabus: syllabusInputEl.value,
+    };
 
     // Använd printUserDetails för att skriva ut användardetaljer
     printCourseDetails(newCourse);
+
+    // Lagra kurs i localStorage
+    storeCourse(newCourse);
+}
+
+function printCourseDetails(course: Course):void {
+    const courseTable = document.querySelector("tbody") as HTMLElement;
+    let tableRow: HTMLElement = document.createElement("tr");
+    tableRow.innerHTML = `
+        <td>${course.code}</td>
+        <td>${course.name}</td>
+        <td>${course.progression}</td>
+        <td>${course.syllabus}</td>
+    `;
+
+    // Skapa knapp för radering av kurs
+    const deleteBtn: HTMLElement = document.createElement("td");
+    deleteBtn.innerHTML = `<span class="material-symbols-outlined remove">delete</span>`;
+    tableRow.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click", () => {
+        tableRow.remove();
+        const courses: Course[] = JSON.parse(localStorage.getItem("courses") || "[]");
+        
+        const index = courses.findIndex(c  => c.code === course.code);
+
+        if (index !== -1) {
+            courses.splice(index, 1);
+            localStorage.setItem("courses", JSON.stringify(courses));
+        }
+    });
+
+    // Lägg till i DOM
+    if(courseTable) {
+        courseTable.appendChild(tableRow);
+    }
+}
+
+function storeCourse(course: Course):void {
+    const storedCourses: Course[] = JSON.parse(localStorage.getItem("courses") || "[]");
+    storedCourses.push(course);
+    localStorage.setItem("courses", JSON.stringify(storedCourses));
 }
 
 function getStoredCourses () {
@@ -88,18 +115,13 @@ function getStoredCourses () {
     const courseArr = JSON.parse(localStorage.getItem("courses") || "[]");
     const courseTable = document.querySelector("tbody") as HTMLElement;
 
+    console.log(courseArr);
 
     if (courseArr && courseTable) {
         courseArr.forEach((course: Course)  => {
-            courseTable.innerHTML += `
-            <tr>
-                <td>${course.code}</td>
-                <td>${course.name}</td>
-                <td>${course.progression}</td>
-                <td>${course.syllabus}</td>
-                <td><span class="material-symbols-outlined remove">delete</span></td>
-            </tr>
-            `
+            printCourseDetails(course);
         });
     }
 }
+
+
